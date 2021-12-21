51c1b5673f5f57b3d8004a316e6962460872a7be
Ellen Li <ellenli@Ellens-MacBook-Pro.local>
2021-12-20 22:04:16 -0500

    init xcodeproj + basic UI

---
+++ GlucoseMonitor/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GlucoseMonitor
+//
+//  Created by Ellen Li on 2021-12-20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
